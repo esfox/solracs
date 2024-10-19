0a1ca39eec4e5f71d8663aa5128c450646d61008
esfox <osairis100@gmail.com>
2024-10-19 21:58:59 +0800

    Add contact section

---
+++ src/components/ContactSection.svelte
@@ -0,0 +1,36 @@
+<section>
+  <h1>
+    less yapping,
+    <div>more creating.</div>
+  </h1>
+  <a href="mailto:sabarillocarloschristian@gmail.com">
+    <button> sabarillocarloschristian@gmail.com </button>
+  </a>
+</section>
+
+<style lang="scss">
+  section {
+    text-align: center;
+    margin-top: 96px;
+    padding-bottom: 58px;
+  }
+
+  h1 {
+    font-weight: 700;
+    font-size: 40px;
+    letter-spacing: -1px;
+  }
+
+  a button {
+    height: 60px;
+    background: var(--black);
+    border: none;
+    border-radius: 1000px;
+    color: var(--white);
+    font-size: 20px;
+    text-decoration: none;
+    padding-inline: 16px;
+    margin-top: 24px;
+    cursor: pointer;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,14 @@
   import WorksSection from '../components/WorksSection.svelte';
   import AboutSection from '../components/AboutSection.svelte';
   import ServicesSection from '../components/ServicesSection.svelte';
+  import ContactSection from '../components/ContactSection.svelte';
 </script>
 
 <HeroSection />
 <WorksSection />
 <AboutSection />
 <ServicesSection />
+<ContactSection />
 
 <style lang="scss">
 </style>
